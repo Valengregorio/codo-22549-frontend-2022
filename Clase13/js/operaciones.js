9869596f3dde639e01ec4c349583808b2732ea69
Valentina Gregorio <valengregorio@gmail.com>
2022-09-16 20:29:08 -0300

    a

---
+++ Clase13/js/operaciones.js
@@ -0,0 +1,20 @@
+function saludar(){
+    alert('hola mundo');
+}
+
+/*OPERADORES*/
+/* + - / * */
+1+2;
+/*lo que ponga con la etiqueta console.log aparece en la consola de fiveserver*/
+console.log(1+2); /*esto va a la consola del navegador*/
+
+/*variable: tienen un nombre y un valor(pueden no tenerlo)--> let op1 = 1
+Permiten guardar valores:
+-Cadena="hola soy Valentina"
+-Numero=100*/
+/*¿que queremos hacer? sumar dos valores y mostrar su resutado*/
+let op1= prompt('Ingrese el valor de op1');
+let op2=2;
+let res=op1+op2
+console.log(res); /*esto va a la consola del navegador*/
+// alert(res) //
